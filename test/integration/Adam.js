40910538065bd950b42fa2b0c80a78cfec355cf2
Allen Wong <allen.wong@apoidea.ai>
2022-01-19 11:41:56 +0800

    gather test cases into same describe group

--- test/integration/Adam.js
+++ test/integration/Adam.js
@@ -1,5 +1,6 @@
 const { expect } = require('chai');
 const { ethers } = require('hardhat');
+const _ = require('lodash');
 
 describe('Create AssetManager', function () {
   let creator;
@@ -62,46 +63,57 @@ describe('Create AssetManager', function () {
     expect(await adam.countPublicStrategies()).to.equal(0);
     expect(await adam.countStrategies()).to.equal(1);
   });
+  describe('Create AssetManager', function () {
+    let strategy, amAddr;
+    beforeEach(async function () {
+      const tx1 = await adam.createAssetManager('AM Ltd');
+      const receipt = await tx1.wait();
+      const creationEventLog = _.find(receipt.events, { event: 'CreateAssetManager' });
+      amAddr = creationEventLog.args.assetManager;
+      const tx2 = await adam.createStrategy(amAddr, 'AM Ltd', false);
+      await tx2.wait();
+      const sAddr = await adam.publicStrategies(0);
 
-  it('create Portfolio when deposit()', async function () {
-    const tx1 = await adam.createAssetManager('AM Ltd');
-    await tx1.wait();
+      strategy = await ethers.getContractAt('Strategy', sAddr);
+    });
 
-    const amAddr = await adam.assetManagers(0);
-    const tx2 = await adam.createStrategy(amAddr, 'AM Ltd', false);
-    await tx2.wait();
-    const sAddr = await adam.publicStrategies(0);
+    it('create Portfolio when deposit()', async function () {
+      await strategy.deposit({ value: ethers.utils.parseEther('0.000123') });
 
-    const strategy = await ethers.getContractAt('Strategy', sAddr);
-    await strategy.deposit({ value: ethers.utils.parseEther('0.000123') });
+      expect(await strategy.balanceOf(creator.address)).to.equal(1);
+      expect(await strategy.countPortfolio()).to.equal(1);
 
-    expect(await strategy.balanceOf(creator.address)).to.equal(1);
-    expect(await strategy.countPortfolio()).to.equal(1);
+      const base64String = (await strategy.tokenURI(1)).split(',')[1];
+      const uriResponse = Buffer.from(base64String, 'base64');
+      const jsonResponse = JSON.parse(uriResponse);
 
-    const base64String = (await strategy.tokenURI(1)).split(',')[1];
-    const uriResponse = Buffer.from(base64String, 'base64');
-    const jsonResponse = JSON.parse(uriResponse);
+      expect(jsonResponse.name).to.equal('Adam Portfolio #1');
+      expect(jsonResponse.attributes[0].value).to.not.be.empty;
+      expect(await ethers.provider.getBalance(amAddr)).to.equal(ethers.utils.parseEther('0.000123'));
+    });
 
-    expect(jsonResponse.name).to.equal('Adam Portfolio #1');
-    expect(jsonResponse.attributes[0].value).to.not.be.empty;
-  });
+    it('gives token uri with portfolio address', async function () {
+      const tx = await strategy.deposit({ value: ethers.utils.parseEther('0.000123') });
+      const receipt = await tx.wait();
+      const creationEventLog = _.find(receipt.events, { event: 'CreatePortfolio' });
+      const portfolioAddr = creationEventLog.args.portfolio;
 
-  it('should not recreate Portfolio when deposit() again by same EOA', async function () {
-    const tx1 = await adam.createAssetManager('AM Ltd');
-    await tx1.wait();
+      const base64String = (await strategy.tokenURI(1)).split(',')[1];
+      const uriResponse = Buffer.from(base64String, 'base64');
+      const jsonResponse = JSON.parse(uriResponse);
 
-    const amAddr = await adam.assetManagers(0);
-    const tx2 = await adam.createStrategy(amAddr, 'AM Ltd', false);
-    await tx2.wait();
-    const sAddr = await adam.publicStrategies(0);
+      expect(jsonResponse.name).to.equal('Adam Portfolio #1');
+      expect(jsonResponse.attributes[0].value.toLowerCase()).to.equal(portfolioAddr.toLowerCase());
+    });
 
-    const strategy = await ethers.getContractAt('Strategy', sAddr);
-    await strategy.deposit({ value: ethers.utils.parseEther('0.000123') });
-    await strategy.deposit({ value: ethers.utils.parseEther('0.000123') });
-    await strategy.deposit({ value: ethers.utils.parseEther('0.000123') });
+    it('should not recreate Portfolio when deposit() again by same EOA', async function () {
+      await strategy.deposit({ value: ethers.utils.parseEther('0.000123') });
+      await strategy.deposit({ value: ethers.utils.parseEther('0.000123') });
+      await strategy.deposit({ value: ethers.utils.parseEther('0.000123') });
 
-    expect(await strategy.balanceOf(creator.address)).to.equal(1);
-    expect(await strategy.countPortfolio()).to.equal(1);
-    expect(await ethers.provider.getBalance(amAddr)).to.equal(ethers.utils.parseEther('0.000369'));
+      expect(await strategy.balanceOf(creator.address)).to.equal(1);
+      expect(await strategy.countPortfolio()).to.equal(1);
+      expect(await ethers.provider.getBalance(amAddr)).to.equal(ethers.utils.parseEther('0.000369'));
+    });
   });
 });
