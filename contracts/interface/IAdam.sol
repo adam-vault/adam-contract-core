c0da44a78ddcc266a096485d0f571d759e1193cb
Eddy Lau <eddy.lau@apoidea.ai>
2022-02-11 17:37:25 +0800

    fix(beacon proxy)

--- contracts/interface/IAdam.sol
+++ contracts/interface/IAdam.sol
@@ -4,6 +4,6 @@ pragma solidity ^0.8.0;
 
 interface IAdam {
     function assetManagerRegistry(address manager) external view returns (bool);
-    function setTreasury(address treasury) external;
+    function setTreasury(address _treasury) external;
     function treasury() external view returns (address);
 }
