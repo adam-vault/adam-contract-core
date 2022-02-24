af4272890011843c04879b0c3f8392fb5767582c
Allen Wong <allen.wong@apoidea.ai>
2022-02-24 16:52:48 +0800

    Add redemption test case

--- test/integration/Adam.js
+++ test/integration/Adam.js
@@ -144,53 +144,57 @@ describe('Create AssetManager', function () {
     });
   });
 
-  // describe('Interact with WETH', function () {
-  //   let strategy, amAddr, assetManager, mockWETH9;
-  //   beforeEach(async function () {
-  //     const MockWETH9 = await ethers.getContractFactory('MockWETH9', creator);
-  //     mockWETH9 = await MockWETH9.deploy();
-  //     await mockWETH9.deployed();
+  describe('Redeem ETH from DAO', function () {
+    let dao, membership;
+    beforeEach(async function () {
+      const tx1 = await adam.createDao('A Company', 'ACOM');
+      const receipt = await tx1.wait();
+      const creationEventLog = _.find(receipt.events, { event: 'CreateDao' });
+      const daoAddr = creationEventLog.args.dao;
+      dao = await ethers.getContractAt('Dao', daoAddr);
+      const membershipAddr = await dao.membership();
+      membership = await ethers.getContractAt('Membership', membershipAddr);
+      await dao.deposit({ value: ethers.utils.parseEther('123') });
+    });
 
-  //     const tx1 = await adam.createAssetManager('AM Ltd');
-  //     const receipt = await tx1.wait();
-  //     const creationEventLog = _.find(receipt.events, { event: 'CreateAssetManager' });
-  //     amAddr = creationEventLog.args.assetManager;
-  //     assetManager = await ethers.getContractAt('AssetManager', amAddr);
-  //     const tx2 = await adam.createStrategy(amAddr, 'AM Ltd', false);
-  //     await tx2.wait();
-  //     const sAddr = await adam.publicStrategies(0);
-  //     strategy = await ethers.getContractAt('Strategy', sAddr);
-  //   });
+    it('redeem and burn exact amount of token', async function () {
+      await dao.redeem(ethers.utils.parseEther('3'));
 
-  //   it('swaps ERC1155 token when exchanges', async function () {
-  //     const [p1, p2, p3] = await Promise.all([owner1, owner2, owner3].map(async (owner) => {
-  //       const tx = await strategy.connect(owner).deposit({ value: ethers.utils.parseEther('0.000123') });
-  //       const receipt = await tx.wait();
-  //       const creationEventLog = _.find(receipt.events, { event: 'CreatePortfolio' });
-  //       return creationEventLog.args.portfolio;
-  //     }));
+      expect(await membership.balanceOf(creator.address)).to.equal(1);
+      const memberAddr = await membership.members(0);
 
-  //     await assetManager.depositAnyContract(ethers.constants.AddressZero, mockWETH9.address,
-  //       [p1, p2, p3],
-  //       [
-  //         ethers.utils.parseEther('0.000123'),
-  //         ethers.utils.parseEther('0.0001'),
-  //         ethers.utils.parseEther('0.0001'),
-  //       ]);
-  //     const base64String = (await assetManager.uri(await assetManager.ethId())).split(',')[1];
-  //     const uriResponse = Buffer.from(base64String, 'base64');
-  //     const jsonResponse = JSON.parse(uriResponse);
-  //     expect(jsonResponse.totalSupply).to.equal(46000000000000);
+      expect(await dao.balanceOf(memberAddr, dao.ethId())).to.equal(ethers.utils.parseEther('120'));
+    });
+  });
 
-  //     expect(await assetManager.balanceOf(p1, 1)).to.equal(ethers.utils.parseEther('0'));
-  //     expect(await assetManager.balanceOf(p1, 2)).to.equal(ethers.utils.parseEther('0.000123'));
-  //     expect(await assetManager.balanceOf(p2, 1)).to.equal(ethers.utils.parseEther('0.000023'));
-  //     expect(await assetManager.balanceOf(p2, 2)).to.equal(ethers.utils.parseEther('0.000100'));
-  //     expect(await assetManager.balanceOf(p3, 1)).to.equal(ethers.utils.parseEther('0.000023'));
-  //     expect(await assetManager.balanceOf(p3, 2)).to.equal(ethers.utils.parseEther('0.000100'));
+  describe('Redeem Token from DAO', function () {
+    let dao, membership, erc20;
+    beforeEach(async function () {
+      const tx1 = await adam.createDao('A Company', 'ACOM');
+      const receipt = await tx1.wait();
+      const creationEventLog = _.find(receipt.events, { event: 'CreateDao' });
+      const daoAddr = creationEventLog.args.dao;
+      dao = await ethers.getContractAt('Dao', daoAddr);
+      const membershipAddr = await dao.membership();
+      membership = await ethers.getContractAt('Membership', membershipAddr);
 
-  //     expect(await mockWETH9.balanceOf(assetManager.address)).to.equal(ethers.utils.parseEther('0.000323'));
-  //   });
-  // });
+      const A = await ethers.getContractFactory('TokenA');
+      erc20 = await A.deploy();
+      await erc20.deployed();
+      await erc20.mint(creator.address, ethers.utils.parseEther('123'));
 
+      await erc20.approve(dao.address, ethers.utils.parseEther('123'));
+      await dao.depositToken(erc20.address, ethers.utils.parseEther('123'));
+    });
+
+    it('redeem and burn exact amount of token', async function () {
+      await dao.redeemToken(erc20.address, ethers.utils.parseEther('3'));
+
+      expect(await membership.balanceOf(creator.address)).to.equal(1);
+      const memberAddr = await membership.members(0);
+
+      expect(await dao.balanceOf(memberAddr, 2)).to.equal(ethers.utils.parseEther('120'));
+      expect(await erc20.balanceOf(creator.address)).to.equal(ethers.utils.parseEther('3'));
+    });
+  });
 });
