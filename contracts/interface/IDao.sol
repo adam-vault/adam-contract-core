0c3ba34bd8960dad7b1dcb2af8a2c87d25212952
Allen Wong <allen.wong@apoidea.ai>
2022-02-28 11:35:39 +0800

    fix interface returns type

--- contracts/interface/IDao.sol
+++ contracts/interface/IDao.sol
@@ -4,5 +4,5 @@ pragma solidity ^0.8.0;
 interface IDao {
     function initialize(address _adam, address _owner, string memory _name, string memory _symbol, address _membership) external;
     function setName(string calldata _name) external;
-    function membership() external view;
+    function membership() external view returns (address);
 }
