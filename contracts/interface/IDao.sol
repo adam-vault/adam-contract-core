4d08253c3529d87df066eb09a15129b2a083886f
Allen Wong <allen.wong@apoidea.ai>
2022-03-02 15:43:15 +0800

    whitelist deposit tokens

--- contracts/interface/IDao.sol
+++ contracts/interface/IDao.sol
@@ -2,7 +2,8 @@
 
 pragma solidity ^0.8.0;
 interface IDao {
-    function initialize(address _adam, address _owner, string memory _name, string memory _symbol, address _membership) external;
+    function initialize(address _adam, address _owner, string memory _name, string memory _symbol, address _membership, uint256 _locktime,
+        address[] calldata _depositTokens) external;
     function setName(string calldata _name) external;
     function membership() external view returns (address);
 }
