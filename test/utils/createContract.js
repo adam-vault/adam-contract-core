663f213240e0cd1752593d643c32906d474b8a1b
Johnson Choi <johnson.choi@apoidea.ai>
2022-03-07 12:06:06 +0800

    fix unit test

--- test/utils/createContract.js
+++ test/utils/createContract.js
@@ -1,8 +1,17 @@
 const { ethers, upgrades } = require('hardhat');
 
+const deployBudgetApprovals = async (signer) => {
+  const TransferERC20BudgetApproval = await ethers.getContractFactory('TransferERC20BudgetApproval', { signer });
+  const transferERC20BudgetApproval = await TransferERC20BudgetApproval.deploy();
+  await transferERC20BudgetApproval.deployed();
+
+  return [transferERC20BudgetApproval.address];
+};
+
 const createAdam = async () => {
   const [creator] = await ethers.getSigners();
 
+  const budgetApprovalsAddress = await deployBudgetApprovals(creator);
   const Dao = await ethers.getContractFactory('Dao', { signer: creator });
   const Membership = await ethers.getContractFactory('Membership', { signer: creator });
   const Adam = await ethers.getContractFactory('Adam', { signer: creator });
@@ -12,7 +21,7 @@ const createAdam = async () => {
   await dao.deployed();
   await membership.deployed();
 
-  const adam = await upgrades.deployProxy(Adam, [dao.address, membership.address], { kind: 'uups' });
+  const adam = await upgrades.deployProxy(Adam, [dao.address, membership.address, budgetApprovalsAddress], { kind: 'uups' });
   await adam.deployed();
   return adam;
 };
