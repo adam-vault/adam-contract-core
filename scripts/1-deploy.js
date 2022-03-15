9ecf124b14592a03548d841536ceffab95a2d017
Eddy Lau <eddy.lau@apoidea.ai>
2022-03-15 14:13:35 +0800

    fix(script)

--- scripts/1-deploy.js
+++ scripts/1-deploy.js
@@ -18,19 +18,27 @@ async function main () {
   const Dao = await hre.ethers.getContractFactory('Dao');
   const Membership = await hre.ethers.getContractFactory('Membership');
   const Adam = await hre.ethers.getContractFactory('Adam');
+  const GovernFactory = await ethers.getContractFactory('GovernFactory');
+  const Govern = await ethers.getContractFactory('Govern');
 
   const dao = await Dao.deploy();
   await dao.deployed();
   const membership = await Membership.deploy();
   await membership.deployed();
+  const governFactory = await GovernFactory.deploy();
+  await governFactory.deployed();
+  const govern = await Govern.deploy();
+  await govern.deployed();
 
-  const adam = await hre.upgrades.deployProxy(Adam, [dao.address, membership.address, budgetApprovalsAddress], { kind: 'uups' });
+  const adam = await hre.upgrades.deployProxy(Adam, [dao.address, membership.address, budgetApprovalsAddress, governFactory.address, govern.address], { kind: 'uups' });
   await adam.deployed();
 
   console.log('dao deployed to: ', dao.address);
   console.log('membership deployed to: ', membership.address);
   console.log('adam deployed to: ', adam.address);
   console.log('budget approvals deployed to: ', budgetApprovalsAddress);
+  console.log('governFactory deployed to', governFactory.address);
+  console.log('govern deployed to', govern.address);
 }
 
 main().catch((error) => {
