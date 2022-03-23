8b73d9b1c02dd9605e9a1e0feab1c33b0aa74993
Allen Wong <allen.wong@apoidea.ai>
2022-03-23 15:00:59 +0800

    generate fake daos

--- scripts/2-create-dao.js
+++ scripts/2-create-dao.js
@@ -1,17 +1,18 @@
 const hre = require('hardhat');
 const _ = require('lodash');
+const { faker } = require('@faker-js/faker');
 
 // rinkeby
 const adamAddress = '0xdacB1Faa1749976aCc37bAbD4aEb97d735B22F7d';
-const DAIAddress = '0xc7AD46e0b8a400Bb3C915120d284AafbA8fc4735';
 
 async function main () {
   const adam = await hre.ethers.getContractAt('Adam', adamAddress);
-  const tx = await adam.createDao('Dao', 'D', 'Description', 10000000, [hre.ethers.constants.AddressZero, DAIAddress]);
-  const receipt = await tx.wait();
-  const creationEventLog = _.find(receipt.events, { event: 'CreateDao' });
-
-  console.log('dao created at:', creationEventLog.args.dao);
+  await Promise.all([0, 100, 4, 500, 10000000].map(async (lockup) => {
+    const tx = await adam.createDao(faker.company.companyName(), faker.commerce.productDescription(), lockup, [13, 3000, 5000], [13, 3000, 5000], [13, 3000, 5000]);
+    const receipt = await tx.wait();
+    const creationEventLog = _.find(receipt.events, { event: 'CreateDao' });
+    console.log('dao created at:', creationEventLog.args.dao);
+  }));
 }
 
 // We recommend this pattern to be able to use async/await everywhere
