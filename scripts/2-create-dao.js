5904eb017a9e3821d3cb67220f9b7350e7bb3113
Allen Wong <allen.wong@apoidea.ai>
2022-03-23 15:08:08 +0800

    fix create dao with replacement fee issue

--- scripts/2-create-dao.js
+++ scripts/2-create-dao.js
@@ -3,16 +3,18 @@ const _ = require('lodash');
 const { faker } = require('@faker-js/faker');
 
 // rinkeby
-const adamAddress = '0xdacB1Faa1749976aCc37bAbD4aEb97d735B22F7d';
+const adamAddress = '0x41B0919976F6120BB1a89659Ba4eC7109A775F11';
 
 async function main () {
   const adam = await hre.ethers.getContractAt('Adam', adamAddress);
-  await Promise.all([0, 100, 4, 500, 10000000].map(async (lockup) => {
+  await [0, 100, 4, 500, 10000000].reduce(async (p, lockup) => {
+    await p;
     const tx = await adam.createDao(faker.company.companyName(), faker.commerce.productDescription(), lockup, [13, 3000, 5000], [13, 3000, 5000], [13, 3000, 5000]);
-    const receipt = await tx.wait();
-    const creationEventLog = _.find(receipt.events, { event: 'CreateDao' });
-    console.log('dao created at:', creationEventLog.args.dao);
-  }));
+    return tx.wait().then((receipt) => {
+      const creationEventLog = _.find(receipt.events, { event: 'CreateDao' });
+      console.log('dao created at:', creationEventLog.args.dao);
+    });
+  }, Promise.resolve());
 }
 
 // We recommend this pattern to be able to use async/await everywhere
