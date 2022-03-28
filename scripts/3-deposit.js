4036f91538fce92e967bba3f2695a292f59e1fe8
Eddy Lau <eddy.lau@apoidea.ai>
2022-03-28 18:22:18 +0800

    feat(add script):delegate

--- scripts/3-deposit.js
+++ scripts/3-deposit.js
@@ -4,8 +4,13 @@ const hre = require('hardhat');
 const daoAddress = '0xa9D36Fe3bDF720e0d1044282fa37386306BCbf96';
 
 async function main () {
+  const [creator] = await ethers.getSigners();
+
   const dao = await hre.ethers.getContractAt('Dao', daoAddress);
   await dao.deposit({ value: hre.ethers.utils.parseEther('0.000345') });
+  const membershipAddr = await dao.membership();
+  const membership = await hre.ethers.getContractAt('Membership', membershipAddr);
+  await membership.delegate(creator.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
