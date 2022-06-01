42b9efa26276c2b97bdfd66242f5575a7a945859
max.choi@apoidea.ai <max.choi@apoidea.ai>
2022-06-01 11:23:48 +0800

    fix test case

--- test/integration/TransferERC20BudgetApproval.js
+++ test/integration/TransferERC20BudgetApproval.js
@@ -2,8 +2,9 @@ const { expect } = require('chai');
 const { ethers } = require('hardhat');
 const _ = require('lodash');
 const findEventArgs = require('../../utils/findEventArgs');
+const paramsStruct = require('../../utils/paramsStruct');
 
-const { createTokens, createAdam, createFeedRegistry, createBudgetApprovals } = require('../utils/createContract');
+const { createTokens, createAdam, createBudgetApprovals } = require('../utils/createContract');
 
 const ETHAddress = '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE';
 const { parseEther } = ethers.utils;
@@ -29,23 +30,9 @@ describe('TransferERC20BudgetApproval.sol', function () {
 
     budgetApprovalAddresses = await createBudgetApprovals(executor);
     adam = await createAdam(feedRegistry, budgetApprovalAddresses);
+
     const tx1 = await adam.createDao(
-      [
-        'A Company', // _name
-        'Description', // _description
-        10000000, // _locktime
-        0, // MemberTokenType
-        '0x0000000000000000000000000000000000000000', // memberToken
-        [13, 3000, 5000, 0], // budgetApproval
-        [13, 3000, 5000, 0], // revokeBudgetApproval
-        [13, 3000, 5000, 0], // general,
-        [13, 3000, 5000, 0], // daoSetting
-        [], // tokenInfo
-        0,
-        0, // minDepositAmount
-        0, // minMemberTokenToJoin
-        [],
-      ],
+      paramsStruct.getCreateDaoParams({}),
     );
     const { dao: daoAddr } = await findEventArgs(tx1, 'CreateDao');
     dao = await ethers.getContractAt('Dao', daoAddr);
