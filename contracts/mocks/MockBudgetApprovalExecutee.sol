3fc8aa3257f2a4d583003f52343d754b4b803524
max.choi@apoidea.ai <max.choi@apoidea.ai>
2022-06-06 10:59:39 +0800

    fix lint

--- contracts/mocks/MockBudgetApprovalExecutee.sol
+++ contracts/mocks/MockBudgetApprovalExecutee.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 
 contract MockBudgetApprovalExecutee {
     mapping(address => bool) public budgetApprovals;
-    address public memberToken ; 
+    address public memberToken; 
     //todo workaround for AS-834 , need to remove it when split memeber token outflow BA
     event CreateBudgetApproval(address budgetApproval, bytes data);
 
