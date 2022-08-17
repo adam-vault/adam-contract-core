5ce79bdce168557241738cb15b2dc4f1da0d41af
Allen Wong <allen.wong@apoidea.ai>
2022-08-17 18:23:07 +0800

    test: increase Team.sol coverage

--- test/unit/Team.js
+++ test/unit/Team.js
@@ -3,13 +3,18 @@ const { ethers, upgrades } = require('hardhat');
 const decodeBase64 = require('../utils/decodeBase64');
 
 describe('Team.sol', function () {
-  let creator, member1, member2, member3, member4;
+  let creator, member1, member2, member3;
   let team;
+  let teamId, tx;
 
   beforeEach(async function () {
-    [creator, member1, member2, member3, member4] = await ethers.getSigners();
+    [creator, member1, member2, member3] = await ethers.getSigners();
     const Team = await ethers.getContractFactory('Team', { signer: creator });
     team = await upgrades.deployProxy(Team, { kind: 'uups' });
+    tx = await team.addTeam('Team Name', member1.address, [member1.address, member2.address], 'Description');
+    const receipt = await tx.wait();
+    const event = receipt.events.find(e => e.event === 'TransferSingle');
+    teamId = event.args.id;
   });
 
   it('init creator as owner', async function () {
@@ -28,33 +33,18 @@ describe('Team.sol', function () {
       const v2Contract = await ethers.getContractAt('MockVersionUpgrade', team.address);
       expect(await v2Contract.v2()).to.equal(true);
     });
-
     it('throws "Ownable: caller is not the owner" error if upgrade by non owner', async function () {
       await expect(team.connect(member2).upgradeTo(mockV2Impl.address)).to.revertedWith('Ownable: caller is not the owner');
     });
   });
 
   describe('safeTransferFrom()', function () {
-    let teamId, tx;
-    beforeEach(async function () {
-      tx = await team.addTeam('Team Name', member1.address, [member1.address, member2.address], 'Description');
-      const receipt = await tx.wait();
-      const event = receipt.events.find(e => e.event === 'TransferSingle');
-      teamId = event.args.id;
-    });
     it('throws "Team: Transfer of team ownership is aboundand" error if member transfer their token', async function () {
       await expect(team.connect(member2).safeTransferFrom(member2.address, member3.address, teamId, 1, '0x')).to.revertedWith('Team: Transfer of team ownership is aboundand');
     });
   });
 
   describe('addTeam()', function () {
-    let teamId, tx;
-    beforeEach(async function () {
-      tx = await team.addTeam('Team Name', member1.address, [member1.address, member2.address], 'Description');
-      const receipt = await tx.wait();
-      const event = receipt.events.find(e => e.event === 'TransferSingle');
-      teamId = event.args.id;
-    });
     it('adds a team', async function () {
       expect(tx).to.emit('AddTeam');
       expect(await team.creatorOf(teamId)).to.eq(creator.address);
@@ -69,13 +59,6 @@ describe('Team.sol', function () {
   });
 
   describe('uri()', function () {
-    let teamId;
-    beforeEach(async function () {
-      const tx = await team.addTeam('Team Name', member1.address, [member1.address, member2.address], 'Description');
-      const receipt = await tx.wait();
-      const event = receipt.events.find(e => e.event === 'TransferSingle');
-      teamId = event.args.id;
-    });
     it('returns uri with name, creator, minter', async function () {
       const uri = await team.uri(teamId);
       const res = decodeBase64(uri);
@@ -87,13 +70,6 @@ describe('Team.sol', function () {
   });
 
   describe('addMembers()', function () {
-    let teamId;
-    beforeEach(async function () {
-      const tx = await team.addTeam('Team Name', member1.address, [member1.address, member2.address], 'Description');
-      const receipt = await tx.wait();
-      const event = receipt.events.find(e => e.event === 'TransferSingle');
-      teamId = event.args.id;
-    });
     it('mints token to members', async function () {
       await team.connect(member1).addMembers([member3.address], teamId);
       expect(await team.balanceOf(member3.address, teamId)).to.eq(1);
@@ -104,38 +80,38 @@ describe('Team.sol', function () {
     it('throw "Team: only selected minter" error if not minter', async function () {
       await expect(team.connect(member2).addMembers([member3.address], teamId)).to.revertedWith('Team: only selected minter');
     });
+    it('throw "Team: only selected minter" error if team not exists', async function () {
+      await expect(team.connect(member2).addMembers([member3.address], 0)).to.revertedWith('Team: only selected minter');
+    });
   });
 
   describe('removeMembers()', function () {
-    let teamId;
-    beforeEach(async function () {
-      const tx = await team.addTeam('Team Name', member1.address, [member1.address, member2.address], 'Description');
-      const receipt = await tx.wait();
-      const event = receipt.events.find(e => e.event === 'TransferSingle');
-      teamId = event.args.id;
-    });
     it('removes members', async function () {
       await team.connect(member1).removeMembers([member1.address], teamId);
       expect(await team.balanceOf(member1.address, teamId)).to.eq(0);
     });
-
+    it('throw "Team: only selected minter" error if removes non-member', async function () {
+      await expect(team.connect(member1).removeMembers([member3.address], teamId)).to.revertedWith('Team: Member/Members not exists');
+    });
     it('throw "Team: only selected minter" error if not minter', async function () {
-      await expect(team.connect(member2).removeMembers([member3.address], teamId)).to.revertedWith('Team: only selected minter');
+      await expect(team.connect(member2).removeMembers([member1.address], teamId)).to.revertedWith('Team: only selected minter');
+    });
+    it('throw "Team: only selected minter" error if team not exists', async function () {
+      await expect(team.connect(member2).removeMembers([member3.address], 0)).to.revertedWith('Team: only selected minter');
     });
   });
 
   describe('setInfo()', function () {
-    let teamId;
-    beforeEach(async function () {
-      const tx = await team.addTeam('Team Name', member1.address, [member1.address, member2.address], 'Description');
-      const receipt = await tx.wait();
-      const event = receipt.events.find(e => e.event === 'TransferSingle');
-      teamId = event.args.id;
-    });
     it('updates name, description', async function () {
       await team.connect(member1).setInfo('newName', 'newDescription', teamId);
       expect(await team.nameOf(teamId)).to.eq('newName');
       expect(await team.descriptionOf(teamId)).to.eq('newDescription');
+    });
+    it('throw "Team: only selected minter" error if not minter', async function () {
+      await expect(team.connect(member2).setInfo('newName', 'newDescription', teamId)).to.revertedWith('Team: only selected minter');
+    });
+    it('throw "Team: only selected minter" error if team not exists', async function () {
+      await expect(team.connect(member2).setInfo('newName', 'newDescription', 0)).to.revertedWith('Team: only selected minter');
     });
   });
 });
