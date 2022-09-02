4cd0ba3ff4d17183a3408c2c67dc04be7ce86813
Johnson Choi <johnson.choi@apoidea.ai>
2022-09-02 15:02:13 +0800

    fix audit comment

--- contracts/Membership.sol
+++ contracts/Membership.sol
@@ -34,22 +34,22 @@ contract Membership is Initializable, UUPSUpgradeable, ERC721VotesUpgradeable {
         _;
     }
 
-    function initialize(address _dao, string memory _name, uint256 _maxMemberLimit) public initializer
+    function initialize(address _dao, string memory _name, uint256 _maxMemberLimit) external initializer
     {
         __ERC721_init(_name.concat(" Membership"), "MS");
         dao = payable(_dao);
         maxMemberLimit = _maxMemberLimit;
     }
 
-    function createMember(address to) public {
+    function createMember(address to) external onlyDao {
+        uint256 _totalSupply = totalSupply;
         require(!isMember[to], "Member already created");
-        require(msg.sender == dao, "access denied");
-        require(totalSupply < maxMemberLimit, "member count exceed limit");
+        require(_totalSupply < maxMemberLimit, "member count exceed limit");
 
         _tokenIds.increment();
         uint256 newId = _tokenIds.current();
         _safeMint(to, newId, "");
-        totalSupply++;
+        totalSupply = _totalSupply + 1;
         isMember[to] = true;
 
         emit CreateMember(to);
@@ -74,11 +74,12 @@ contract Membership is Initializable, UUPSUpgradeable, ERC721VotesUpgradeable {
     function _beforeTokenTransfer(
       address from,
       address to,
-      uint256
-    ) internal pure override {
+      uint256 tokenId
+    ) internal override {
         if (from != address(0) && to != address(0)) {
 		    revert("Membership: Transfer of membership is aboundand");
-		} 
+		}
+        super._beforeTokenTransfer(from, to, tokenId); 
     }
 
     function _afterTokenTransfer(
