487997432624322e3e1c255b9e6d61e691c0ae90
Eddy Lau <eddy.lau@apoidea.ai>
2022-09-07 17:00:43 +0800

    fix: floating version

--- contracts/Govern.sol
+++ contracts/Govern.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity 0.8.2;
+pragma solidity 0.8.7;
 
 import "@openzeppelin/contracts-upgradeable/governance/GovernorUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
