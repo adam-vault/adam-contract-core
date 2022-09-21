3f83b826938acecbe6a73d3b14d338644d42ba2e
Johnson Choi <johnson.choi@apoidea.ai>
2022-09-21 14:58:46 +0800

    update test case

--- utils/paramsStruct.js
+++ utils/paramsStruct.js
@@ -177,14 +177,14 @@ function getCreateTransferLiquidErc20TokenBAParams ({
   });
 }
 
-function getCreateUniswapBAParams ({
+function getCreateLiquidUniswapBAParams ({
   executor,
   executorTeamId,
   approvers,
   approverTeamId,
   minApproval,
-  text = 'Uniswap',
-  transactionType = 'swap',
+  text = 'Liquid Uniswap',
+  transactionType = 'swapLiquid',
   startTime,
   endTime,
   allowUnlimitedUsageCount,
@@ -220,6 +220,54 @@ function getCreateUniswapBAParams ({
     totalAmount,
     amountPercentage,
     baseCurrency,
+  }).map(([key, value]) => {
+    return value;
+  });
+}
+
+function getCreateUniswapBAParams ({
+  executor,
+  executorTeamId,
+  approvers,
+  approverTeamId,
+  minApproval,
+  text = 'Uniswap',
+  transactionType = 'swap',
+  startTime,
+  endTime,
+  allowUnlimitedUsageCount,
+  usageCount,
+  allowAllFromTokens,
+  fromToken = ethers.constants.AddressZero,
+  allowAllToTokens,
+  toTokens = [],
+  allowAnyAmount,
+  totalAmount = 0,
+  amountPercentage = 100,
+  team,
+}) {
+  return Object.entries({
+    params: getCreateCommonBudgetApprovalParams({
+      executor,
+      executorTeamId,
+      approvers,
+      approverTeamId,
+      minApproval,
+      text,
+      transactionType,
+      startTime,
+      endTime,
+      allowUnlimitedUsageCount,
+      usageCount,
+      team,
+    }),
+    allowAllFromTokens: allowAllFromTokens ?? fromToken === ethers.constants.AddressZero,
+    fromToken,
+    allowAllToTokens: allowAllToTokens ?? toTokens.length === 0,
+    toTokens,
+    allowAnyAmount: allowAnyAmount ?? totalAmount === 0,
+    totalAmount,
+    amountPercentage,
   }).map(([key, value]) => {
     return value;
   });
@@ -302,11 +350,13 @@ function getCreateCommonBudgetApprovalParams ({
     return value;
   });
 }
+
 module.exports = {
   getCreateDaoParams,
   getCreateDaoParamsV2,
   getCreateTransferERC20BAParams,
   getCreateTransferLiquidErc20TokenBAParams,
-  getCreateUniswapBAParams,
+  getCreateLiquidUniswapBAParams,
   getCreateTransferERC721BAParams,
+  getCreateUniswapBAParams,
 };
