d1037eb48445d0b5ba965eba80eb083f98d7287d
Johnson Choi <johnson.choi@apoidea.ai>
2022-09-28 15:29:56 +0800

    fix deploy script missing

--- deploy/04_deploy_adam.js
+++ deploy/04_deploy_adam.js
@@ -19,6 +19,7 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
       get('LiquidUniswapBudgetApproval'),
       get('TransferERC721BudgetApproval'),
       get('TransferERC20BudgetApproval'),
+      get('UniswapBudgetApproval'),
     ])
   ).map((deployment) => deployment.address);
 
@@ -58,6 +59,7 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     liquidUniswapBudgetApproval: budgetApprovalsAddress[1],
     transferErc721BudgetApproval: budgetApprovalsAddress[2],
     transferERC20BudgetApproval: budgetApprovalsAddress[3],
+    uniswapBudgetApproval: budgetApprovalsAddress[4],
     team: team.address,
   };
 
