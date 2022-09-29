8ee6a67a35832e6c90a32f47dd02b7d10bcd164f
ayame30 <ayame30@users.noreply.github.com>
2022-09-29 03:43:57 +0000

    [Auto Commit] Sync v0.0.97-predev deployments

--- contracts/lib/Constant.sol
+++ contracts/lib/Constant.sol
@@ -2,9 +2,9 @@
 pragma solidity 0.8.7;
 
 library Constant {
-    address public constant WETH_ADDRESS = 0xc778417E063141139Fce010982780140Aa0cD5Ab;
+    address public constant WETH_ADDRESS = 0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6;
     address public constant UNISWAP_ROUTER = 0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45;
-    address public constant FEED_REGISTRY = 0xf948fC3D6c2c2C866f622c79612bB4E8708883cF;
+    address public constant FEED_REGISTRY = 0xaBC30D61D30f19de38897EBA18252868B3459664;
     uint public constant BLOCK_NUMBER_IN_SECOND = 13;
     uint public constant STALE_PRICE_DELAY = 86400;
 }
