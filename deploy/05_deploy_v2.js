155504b53de470578cdef5bf25a42765a70bdf17
Allen Wong <allen.wong@apoidea.ai>
2022-09-29 14:14:26 +0800

    Update 05_deploy_v2.js

--- deploy/05_deploy_v2.js
+++ deploy/05_deploy_v2.js
@@ -2,67 +2,67 @@ const { ethers } = require('hardhat');
 const fileReader = require('../utils/fileReader');
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
-  // const { deploy, execute, get } = deployments;
-  // const { deployer } = await getNamedAccounts();
-  // const deployNetwork = hre.network.name || 'kovan';
-  // const adamDeployment = await get('Adam');
-  // const governFactoryDeployment = await get('GovernFactory');
+  const { deploy, execute, get } = deployments;
+  const { deployer } = await getNamedAccounts();
+  const deployNetwork = hre.network.name || 'kovan';
+  const adamDeployment = await get('Adam');
+  const governFactoryDeployment = await get('GovernFactory');
 
-  // const adamV2Implementation = await deploy('AdamV2', { from: deployer, log: true, skipIfAlreadyDeployed: true, gasLimit: 3000000 });
-  // if (adamV2Implementation.newlyDeployed) {
-  //   await execute('Adam', { from: deployer, log: true }, 'upgradeTo', adamV2Implementation.address);
-  // }
+  const adamV2Implementation = await deploy('AdamV2', { from: deployer, log: true, skipIfAlreadyDeployed: true, gasLimit: 3000000 });
+  if (adamV2Implementation.newlyDeployed) {
+    await execute('Adam', { from: deployer, log: true }, 'upgradeTo', adamV2Implementation.address);
+  }
 
-  // const daoV2 = await deploy('DaoV2', { from: deployer, log: true, skipIfAlreadyDeployed: true, gasLimit: 5000000 });
-  // if (daoV2.newlyDeployed) {
-  //   const adam = await ethers.getContractAt('Adam', adamDeployment.address);
-  //   const governFactory = await ethers.getContractAt('GovernFactory', governFactoryDeployment.address);
+  const daoV2 = await deploy('DaoV2', { from: deployer, log: true, skipIfAlreadyDeployed: true, gasLimit: 5000000 });
+  if (daoV2.newlyDeployed) {
+    const adam = await ethers.getContractAt('Adam', adamDeployment.address);
+    const governFactory = await ethers.getContractAt('GovernFactory', governFactoryDeployment.address);
 
-  //   const membershipImplementation = await adam.membershipImplementation();
-  //   const liquidPoolImplementation = await adam.liquidPoolImplementation();
-  //   const memberTokenImplementation = await adam.memberTokenImplementation();
-  //   const governImplementation = await governFactory.governImplementation();
+    const membershipImplementation = await adam.membershipImplementation();
+    const liquidPoolImplementation = await adam.liquidPoolImplementation();
+    const memberTokenImplementation = await adam.memberTokenImplementation();
+    const governImplementation = await governFactory.governImplementation();
 
-  //   await execute('Adam', { from: deployer, log: true }, 'upgradeImplementations',
-  //     daoV2.address,
-  //     membershipImplementation,
-  //     liquidPoolImplementation,
-  //     memberTokenImplementation,
-  //     governImplementation,
-  //     process.env.LATEST_VERSION || 'v2.0.0'
-  //   );
+    await execute('Adam', { from: deployer, log: true }, 'upgradeImplementations',
+      daoV2.address,
+      membershipImplementation,
+      liquidPoolImplementation,
+      memberTokenImplementation,
+      governImplementation,
+      process.env.LATEST_VERSION || 'v2.0.0'
+    );
 
-  //   const budgetApprovalsAddress = (await Promise.all([
-  //     get('TransferLiquidERC20BudgetApproval'),
-  //     get('UniswapBudgetApproval'),
-  //     get('TransferERC721BudgetApproval'),
-  //     get('TransferERC20BudgetApproval'),
-  //   ])).map((deployment) => deployment.address);
+    const budgetApprovalsAddress = (await Promise.all([
+      get('TransferLiquidERC20BudgetApproval'),
+      get('UniswapBudgetApproval'),
+      get('TransferERC721BudgetApproval'),
+      get('TransferERC20BudgetApproval'),
+    ])).map((deployment) => deployment.address);
   
-  //   const contractAddresses = {
-  //     adam: adamDeployment.address,
-  //     dao: daoV2.address,
-  //     membership: membershipImplementation,
-  //     governFactory: liquidPoolImplementation,
-  //     govern: governImplementation,
-  //     memberToken: memberTokenImplementation,
-  //     liquidPool: liquidPoolImplementation,
-  //     transferLiquidERC20BudgetApproval: budgetApprovalsAddress[0],
-  //     uniswapBudgetApproval: budgetApprovalsAddress[1],
-  //     transferErc721BudgetApproval: budgetApprovalsAddress[2],
-  //     transferERC20BudgetApproval: budgetApprovalsAddress[3],
-  //     team: (await get('Team')).address,
-  //   };
+    const contractAddresses = {
+      adam: adamDeployment.address,
+      dao: daoV2.address,
+      membership: membershipImplementation,
+      governFactory: liquidPoolImplementation,
+      govern: governImplementation,
+      memberToken: memberTokenImplementation,
+      liquidPool: liquidPoolImplementation,
+      transferLiquidERC20BudgetApproval: budgetApprovalsAddress[0],
+      uniswapBudgetApproval: budgetApprovalsAddress[1],
+      transferErc721BudgetApproval: budgetApprovalsAddress[2],
+      transferERC20BudgetApproval: budgetApprovalsAddress[3],
+      team: (await get('Team')).address,
+    };
   
-  //   console.log(contractAddresses);
+    console.log(contractAddresses);
   
-  //   fileReader.save('deploy-results', 'results.json', {
-  //     network: deployNetwork,
-  //     block_number: adamDeployment.receipt.blockNumber,
-  //     addresses: contractAddresses,
-  //     initdata_addresses: {},
-  //   });
-  // }
+    fileReader.save('deploy-results', 'results.json', {
+      network: deployNetwork,
+      block_number: adamDeployment.receipt.blockNumber,
+      addresses: contractAddresses,
+      initdata_addresses: {},
+    });
+  }
 };
 
 module.exports.tags = [
