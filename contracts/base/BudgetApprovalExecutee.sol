d84c37255a60016fafaccffe25e785f336e26113
max.choi@apoidea.ai <max.choi@apoidea.ai>
2022-11-09 12:05:06 +0800

    Spilt price resolver logic into price Router
    (cherry picked from commit 004be6a75a135207c1a4c90d4311bf01396e0248)

--- contracts/base/BudgetApprovalExecutee.sol
+++ contracts/base/BudgetApprovalExecutee.sol
@@ -15,6 +15,9 @@ contract BudgetApprovalExecutee is Initializable {
 
     mapping(address => bool) private _budgetApprovals;
 
+    //v2
+    address private _priceRouter;
+
     event CreateBudgetApproval(address budgetApproval, bytes data);
     event ExecuteByBudgetApproval(address budgetApproval, bytes data);
     event RevokeBudgetApproval(address budgetApproval);
@@ -29,12 +32,17 @@ contract BudgetApprovalExecutee is Initializable {
       _disableInitializers();
     }
 
-    function ___BudgetApprovalExecutee_init(address __team) internal onlyInitializing {
+    function ___BudgetApprovalExecutee_init(address __team, address __priceRouter) internal onlyInitializing {
         _team = __team;
+        _priceRouter = __priceRouter;
     }
 
     function team() public view virtual returns (address) {
         return _team;
+    }
+
+    function priceRouter() public view virtual returns (address) {
+        return _priceRouter;
     }
 
     function budgetApprovals(address template) public view virtual returns (bool) {
@@ -79,5 +87,5 @@ contract BudgetApprovalExecutee is Initializable {
         }
     }
 
-    uint256[50] private __gap;
+    uint256[49] private __gap;
 }
--- contracts/base/PriceResolver.sol
+++ contracts/base/PriceResolver.sol
@@ -7,148 +7,54 @@ import "@chainlink/contracts/src/v0.8/interfaces/FeedRegistryInterface.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "../lib/Constant.sol";
+import "../interface/IPriceRouter.sol";
 
 contract PriceResolver is Initializable {
     address private _baseCurrency;
+
+    //v2
+    IPriceRouter private _priceRouter;
 
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
       _disableInitializers();
     }
 
-    function __PriceResolver_init(address __baseCurrency) internal onlyInitializing {
+    function __PriceResolver_init(address __priceRouter, address __baseCurrency) internal onlyInitializing {
+        _priceRouter = IPriceRouter(__priceRouter);
         _baseCurrency = __baseCurrency;
+}
+
+    modifier priceRouterExist {
+        require(address(_priceRouter) != address(0), "Price router not Exist");
+        _;
     }
 
-    function baseCurrency() public view virtual returns (address) {
+    function baseCurrency() public view virtual priceRouterExist returns (address) {
         return _baseCurrency;
     }
-    function _WETH9() internal pure returns (address) {
-        return Constant.WETH_ADDRESS;
+
+    /// @notice This function is imported by other contract, thus cannot be external
+    function assetBaseCurrencyPrice(address asset, uint256 amount) public view virtual priceRouterExist returns (uint256) {
+        return _priceRouter.assetBaseCurrencyPrice(asset, amount, _baseCurrency);
+    }
+
+    function ethAssetPrice(address asset, uint256 ethAmount) public view virtual priceRouterExist returns (uint256) {
+        return _priceRouter.ethAssetPrice(asset, ethAmount);
+    }
+
+    function assetEthPrice(address asset, uint256 amount) public view virtual priceRouterExist returns (uint256) {
+        return _priceRouter.assetEthPrice(asset, amount);
+    }
+
+    function baseCurrencyDecimals() public view virtual priceRouterExist returns (uint8) {
+        return _priceRouter.baseCurrencyDecimals(_baseCurrency);
     }
 
     /// @notice This function is imported by other contract, thus cannot be external
-    function assetBaseCurrencyPrice(address asset, uint256 amount) public view virtual returns (uint256) {
-        address __baseCurrency = baseCurrency();
-        if (asset == __baseCurrency)
-            return amount;
-        
-        if(__baseCurrency == Denominations.ETH || __baseCurrency == _WETH9()) {
-            return assetEthPrice(asset, amount);
-        }
-
-        if(asset == Denominations.ETH || asset == _WETH9()) {
-            return ethAssetPrice(__baseCurrency, amount);
-        }
-
-        uint8 baseDecimals = baseCurrencyDecimals();
-        int price = getDerivedPrice(asset, __baseCurrency, 18 /* ETH decimals */);
-
-        if (price > 0) {
-            return uint256(scalePrice(int256(price) * int256(amount), 18 + IERC20Metadata(asset).decimals(), baseDecimals));
-        }
-        return 0;
+    function canResolvePrice(address asset) public view virtual priceRouterExist returns (bool) {
+        return _priceRouter.canResolvePrice(asset);
     }
 
-    function ethAssetPrice(address asset, uint256 ethAmount) public view virtual returns (uint256) {
-        if (asset == Denominations.ETH || asset == _WETH9())
-            return ethAmount;
-
-        (uint80 roundID, int price, , uint256 updatedAt, uint80 answeredInRound) = 
-        FeedRegistryInterface(Constant.FEED_REGISTRY).latestRoundData(asset, Denominations.ETH);
-        uint8 priceDecimals = FeedRegistryInterface(Constant.FEED_REGISTRY).decimals(asset, Denominations.ETH);
-
-        require(answeredInRound >= roundID, "Stale price in Chainlink");
-        require(block.timestamp <= updatedAt + Constant.STALE_PRICE_DELAY, "Stale price in Chainlink");
-
-        price = scalePrice(price, priceDecimals, 18 /* ETH decimals */);
-        if (price > 0) {
-            return ethAmount * (10 ** IERC20Metadata(asset).decimals()) / uint256(price);
-        }
-
-        return 0;
-    }
-
-    function assetEthPrice(address asset, uint256 amount) public view virtual returns (uint256) {
-        if (asset == Denominations.ETH || asset == _WETH9())
-            return amount;
-
-        (uint80 roundID, int price, , uint256 updatedAt, uint80 answeredInRound) = 
-        FeedRegistryInterface(Constant.FEED_REGISTRY).latestRoundData(asset, Denominations.ETH);
-        uint8 priceDecimals = FeedRegistryInterface(Constant.FEED_REGISTRY).decimals(asset, Denominations.ETH);
-        
-        require(answeredInRound >= roundID, "Stale price in Chainlink");
-        require(block.timestamp <= updatedAt + Constant.STALE_PRICE_DELAY, "Stale price in Chainlink");
-
-        price = scalePrice(price, priceDecimals, 18 /* ETH decimals */);
-
-        if (price > 0) {
-            return uint256(price) * amount / 10 ** IERC20Metadata(asset).decimals();
-        }
-
-        return 0;
-    }
-
-    function getDerivedPrice(address _base, address _quote, uint8 _decimals)
-        internal
-        view
-        virtual
-        returns (int256)
-    {
-        require(_decimals > uint8(0) && _decimals <= uint8(18), "Invalid _decimals");
-        int256 decimals = int256(10 ** uint256(_decimals));
-        (uint80 _baseRoundID, int basePrice, , uint256 _baseUpdatedAt, uint80 _baseAnsweredInRound) = 
-            FeedRegistryInterface(Constant.FEED_REGISTRY).latestRoundData(_base, Denominations.ETH);
-
-        require(_baseAnsweredInRound >= _baseRoundID, "Stale price in Chainlink 104");
-        require(block.timestamp <= _baseUpdatedAt + Constant.STALE_PRICE_DELAY, "Stale price in Chainlink 105");
-
-        uint8 baseDecimals = FeedRegistryInterface(Constant.FEED_REGISTRY).decimals(_base, Denominations.ETH);
-        basePrice = scalePrice(basePrice, baseDecimals, _decimals);
-        (uint80 _quoteRoundID, int quotePrice, , uint256 _quoteUpdatedAt, uint80 _quoteAnsweredInRound) = 
-            FeedRegistryInterface(Constant.FEED_REGISTRY).latestRoundData(_quote, Denominations.ETH);
-        require(_quoteAnsweredInRound >= _quoteRoundID, "Stale price in Chainlink 112");
-        require(block.timestamp <= _quoteUpdatedAt + Constant.STALE_PRICE_DELAY, "Stale price in Chainlink 113");
-
-        uint8 quoteDecimals = FeedRegistryInterface(Constant.FEED_REGISTRY).decimals(_quote, Denominations.ETH);
-        quotePrice = scalePrice(quotePrice, quoteDecimals, _decimals);
-
-        return basePrice * decimals / quotePrice;
-    }
-
-    function scalePrice(int256 _price, uint8 _priceDecimals, uint8 _decimals)
-        internal
-        pure
-        virtual
-        returns (int256)
-    {
-        if (_priceDecimals < _decimals) {
-            return _price * int256(10 ** uint256(_decimals - _priceDecimals));
-        } else if (_priceDecimals > _decimals) {
-            return _price / int256(10 ** uint256(_priceDecimals - _decimals));
-        }
-        return _price;
-    }
-
-    function baseCurrencyDecimals() public view virtual returns (uint8) {
-        if (baseCurrency() == Denominations.ETH) return 18;
-        try IERC20Metadata(baseCurrency()).decimals() returns (uint8 _decimals) {
-            return _decimals;
-        } catch {
-            return 0;
-        }
-    }
-
-    /// @notice This function is imported by other contract, thus cannot be external
-    function canResolvePrice(address asset) public view virtual returns (bool) {
-        if (asset == Denominations.ETH || asset == _WETH9())
-            return true;
-        try FeedRegistryInterface(Constant.FEED_REGISTRY).getFeed(asset, Denominations.ETH) {
-            return true;
-        } catch (bytes memory) {
-            return false;
-        }
-    }
-
-    uint256[50] private __gap;
+    uint256[49] private __gap;
 }
