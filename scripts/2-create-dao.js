2977ae81c8e77bcea19030198362165d9728d4fa
Allen Wong <allen.wong@apoidea.ai>
2022-11-09 18:13:15 +0800

    [AS-1703] feat: update srcipt for deposit and createDao

--- scripts/2-create-dao.js
+++ scripts/2-create-dao.js
@@ -1,67 +1,55 @@
 const hre = require('hardhat');
-const fileReader = require('../utils/fileReader');
+const ethers = require('ethers');
+const inquirer = require('inquirer');
 const paramsStruct = require('../utils/paramsStruct');
-const deploymentResult = fileReader.load('deploy-results/results.json', 'utf8');
-const findEventArgs = require('../utils/findEventArgs');
-
-console.log(deploymentResult);
-const adamAddress = deploymentResult.addresses.adam;
-const deployNetwork = deploymentResult.network;
-const {
-  ETH_ADDRESS, DAI_ADDRESS, USDC_ADDRESS, USDT_ADDRESS,
-} = fileReader.load(`constant/${deployNetwork}.json`, 'utf-8');
-
-const daoAddresses = [];
-const testingDataSet = [{
-  lockTime: 0,
-  mintMemberToken: true,
-  admissionTokens: [[hre.ethers.constants.AddressZero, 0, 0, true]],
-  description: '0 lockup, Mint Member Token, No Admission',
-}, {
-  lockTime: 100,
-  mintMemberToken: false,
-  admissionTokens: [],
-  description: '100 lockup, No Member Token, No Admission',
-}, {
-  lockTime: 100,
-  mintMemberToken: false,
-  admissionTokens: [['0xf9559a051478c6a01f20b3c92438023921de51df', 1, 0, false]],
-  description: '100 lockup, No Member Token, ERC721 MT',
-}, {
-  lockTime: 100,
-  mintMemberToken: false,
-  admissionTokens: [['0xD9BA894E0097f8cC2BBc9D24D308b98e36dc6D02', 1, 0, false]],
-  description: '100 lockup, No Member Token, ERC20 MT',
-},
-];
-
+const ETH = '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee';
 async function main () {
-  const adam = await hre.ethers.getContractAt('Adam', adamAddress);
+  inquirer
+    .prompt([
+      { type: 'input', name: 'adamAddress', message: 'Adam contract address?', default: '0x987747FC7D299500EeaC00B59554515a6E3bBA3f' },
+      { type: 'input', name: 'name', message: 'Dao name?', default: 'New Dao' },
+      { type: 'input', name: 'description', message: 'Dao description?', default: 'New description' },
+      { type: 'input', name: 'logoCID', message: 'Dao logo CID?', default: '' },
+      { type: 'number', name: 'lockTime', message: 'Dao locktime? (in seconds)', default: 0 },
+      { type: 'confirm', name: 'needMemberToken', message: 'Create Member token?' },
 
-  await testingDataSet.reduce(async (p, { lockTime, mintMemberToken, admissionToken, description }) => {
-    await p;
-    const tx = await adam.createDao(paramsStruct.getCreateDaoParams({
-      mintMemberToken,
-      admissionToken,
-      lockTime,
-      depositTokens: [ETH_ADDRESS, DAI_ADDRESS, USDC_ADDRESS, USDT_ADDRESS],
-      baseCurrency: ETH_ADDRESS,
-    }));
+      { type: 'input', name: 'tokenName', message: 'Member token name?', default: 'Dao Member Token', when: (answers) => answers.needMemberToken },
+      { type: 'input', name: 'tokenSymbol', message: 'Member token symbol?', default: 'DMT', when: (answers) => answers.needMemberToken },
+      { type: 'number', name: 'tokenAmount', message: 'Member token mint to dao amount?', default: 1000, when: (answers) => answers.needMemberToken },
 
-    const { dao } = await findEventArgs(tx, 'CreateDao');
-    daoAddresses.push({ address: dao, description });
-  }, Promise.resolve());
+      { type: 'input', name: 'maxMemberLimit', message: 'Max member allowed?', default: ethers.constants.MaxUint256.toString() },
 
-  const governFactory = await hre.ethers.getContractAt('GovernFactory', deploymentResult.addresses.governFactory);
-  const dao0BudgetGovern = await governFactory.governMap(daoAddresses[0].address, 'BudgetApproval');
-  fileReader.save('deploy-results', 'results.json', {
-    ...deploymentResult,
-    initdata_addresses: {
-      ...deploymentResult.initdata_addresses,
-      daos: daoAddresses,
-      dao0BudgetGovern,
-    },
-  });
+      { type: 'checkbox', name: 'depositTokens', message: 'Deposit Tokens?', default: [ETH], choices: [ETH] },
+      { type: 'list', name: 'baseCurrency', message: 'BaseCurrency?', default: ETH, choices: [ETH] },
+      { type: 'number', name: 'minDepositAmount', message: 'Min Deposit Amount?', default: 0 },
+
+      { type: 'number', name: 'generalGovernSetting.0', message: 'Govern proposal duration? (in second)', default: 0 },
+      { type: 'number', name: 'generalGovernSetting.1', message: 'Govern proposal quorum (input 3000 as 30.00%)?', default: 3000 },
+      { type: 'number', name: 'generalGovernSetting.2', message: 'Govern proposal pass threshold (input 3000 as 30.00%)?', default: 5000 },
+      {
+        type: 'list',
+        name: 'generalGovernSetting.3',
+        message: 'Govern proposal vote type?',
+        default: 0,
+        choices: [
+          { value: 0, name: 'Membership' },
+          { value: 1, name: 'MemberToken' },
+          { value: 2, name: 'ExistingToken' },
+        ],
+      },
+      {
+        type: 'input',
+        name: 'generalGovernSetting.4',
+        message: 'Govern proposal external vote token?',
+        default: ethers.constants.AddressZero,
+      },
+      { type: 'number', name: 'generalGovernSetting.5', message: 'Govern proposal duration? (in block)', default: 600 },
+    ])
+    .then(async (answers) => {
+      const adam = await hre.ethers.getContractAt('Adam', answers.adamAddress);
+      const tx = await adam.createDao(...paramsStruct.getCreateDaoParams(answers));
+      console.log(tx);
+    });
 }
 
 // We recommend this pattern to be able to use async/await everywhere
--- scripts/3-deposit.js
+++ scripts/3-deposit.js
@@ -1,17 +1,73 @@
 const hre = require('hardhat');
-const fileReader = require('../utils/fileReader');
-const deploymentResult = fileReader.load('deploy-results/results.json', 'utf8');
+const ethers = require('ethers');
+const inquirer = require('inquirer');
 
-// rinkeby
-const daoAddress = deploymentResult.initdata_addresses.daos[0].address;
+const toBigNumber = (val, decimals) => {
+  if (val._isBigNumber) {
+    return val;
+  }
+
+  const [integerStr, decimalStr = ''] = val.split('.');
+  const bnStr = integerStr + decimalStr.substring(0, decimals).padEnd(decimals, '0');
+  return ethers.BigNumber.from(bnStr);
+};
 
 async function main () {
-  const dao = await hre.ethers.getContractAt('Dao', daoAddress);
-  const lpAddress = await dao.liquidPool();
+  inquirer
+    .prompt([
+      { type: 'input', name: 'daoAddress', message: 'Dao contract address?', default: '0xC6e1Ca4b8e9ceAa7A0cC82Cc13A6F991bcB48453' },
+      {
+        type: 'list',
+        name: 'dest',
+        message: 'To where?',
+        default: 0,
+        choices: ['Treasury', 'LiquidPool'],
+      },
 
-  const lp = await hre.ethers.getContractAt('LiquidPool', lpAddress);
+      {
+        type: 'list',
+        name: 'token',
+        message: 'What kind of deposit?',
+        default: 0,
+        choices: ['ERC20', 'ETH'],
+      },
+      { type: 'input', name: 'tokenAddress', message: 'ERC20 Address?', when: ({ token }) => token === 'ERC20' },
+      { type: 'input', name: 'amount', message: 'How much?', default: '0.001' },
+    ])
+    .then(async ({
+      dest, tokenAddress, token, daoAddress, amount
+    }) => {
+      if (dest === 'Treasury') {
+        if (token === 'ETH') {
+          const [signer] = await hre.ethers.getSigners();
+          return signer.sendTransaction({
+            to: daoAddress,
+            value: ethers.utils.parseEther(amount),
+          });
+        } else {
+          const erc20 = await hre.ethers.getContractAt('ERC20', tokenAddress);
+          const decimals = await erc20.decimals();
+          return erc20.transfer(daoAddress, toBigNumber(amount, decimals));
+        }
+      } else {
+        const dao = await hre.ethers.getContractAt('Dao', daoAddress);
+        const lpAddress = await dao.liquidPool();
+        const lp = await hre.ethers.getContractAt('LiquidPool', lpAddress);
 
-  await lp.deposit({ value: hre.ethers.utils.parseEther('0.000345') });
+        if (token === 'ETH') {
+          return lp.deposit({ value: hre.ethers.utils.parseEther(amount) });
+        } else {
+          const erc20 = await hre.ethers.getContractAt('ERC20', tokenAddress);
+          const decimals = await erc20.decimals();
+          const amountBN = toBigNumber(amount, decimals);
+          await erc20.approve(lpAddress, amountBN);
+          return erc20.depositToken(tokenAddress, amountBN);
+        }
+      }
+    })
+    .then((tx) => {
+      console.log(tx);
+    });
 }
 
 // We recommend this pattern to be able to use async/await everywhere
