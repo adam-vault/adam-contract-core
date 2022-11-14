e8908fb4d4fe92b4067507ae20fdb75e59e339df
Allen Wong <allen.wong@apoidea.ai>
2022-11-14 14:14:36 +0800

    [AS-1703] feat: add Adam L2 address to const

--- contracts/lib/Constant.sol
+++ contracts/lib/Constant.sol
@@ -11,6 +11,7 @@ library Constant {
     address public constant ARBITRUM_L1_OUTBOX = 0x0B9857ae2D4A3DBe74ffE1d7DF045bb7F96E4840;
     address public constant ARBITRUM_L1_GATEWAY_ROUTER = 0x72Ce9c846789fdB6fC1f34aC4AD25Dd9ef7031ef;
     address public constant ARBITRUM_L2_GATEWAY_ROUTER = address(0);
+    address public constant ARBITRUM_L2_ADAM = address(0);
 
     uint public constant BLOCK_NUMBER_IN_SECOND = 12;
     uint public constant STALE_PRICE_DELAY = 86400;
