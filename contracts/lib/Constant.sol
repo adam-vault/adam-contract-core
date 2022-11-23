b6b658f3b4756f3e211e8227c129206528a1d468
Allen Wong <allen.wong@apoidea.ai>
2022-11-23 12:32:49 +0800

    [AS-1703] update: undo Constant.sol

--- contracts/lib/Constant.sol
+++ contracts/lib/Constant.sol
@@ -2,10 +2,10 @@
 pragma solidity 0.8.7;
 
 library Constant {
-    address public constant WETH_ADDRESS = 0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6;
+    address public constant WETH_ADDRESS = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
     address public constant UNISWAP_ROUTER = 0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45;
-    address public constant FEED_REGISTRY = 0xaBC30D61D30f19de38897EBA18252868B3459664;
+    address public constant FEED_REGISTRY = 0x47Fb2585D2C56Fe188D0E6ec628a38b74fCeeeDf;
     address public constant BRIDGE_CURRENCY = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
-    uint public constant BLOCK_NUMBER_IN_SECOND = 13;
+    uint public constant BLOCK_NUMBER_IN_SECOND = 12;
     uint public constant STALE_PRICE_DELAY = 86400;
 }
