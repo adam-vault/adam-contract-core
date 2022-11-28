5b060ec4c99235360d3e592980e9831ed9e3a674
max.choi@apoidea.ai <max.choi@apoidea.ai>
2022-11-28 11:57:27 +0800

    update maxFeePerGas

--- deploy/02_deploy_adam_dependencies.js
+++ deploy/02_deploy_adam_dependencies.js
@@ -2,10 +2,10 @@
 module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
-  await deploy('Dao', { from: deployer, log: true, skipIfAlreadyDeployed: true, gasLimit: 7000000, maxPriorityFeePerGas: 5000000 });
-  await deploy('Membership', { from: deployer, log: true, skipIfAlreadyDeployed: true, gasLimit: 6000000, maxPriorityFeePerGas: 5000000 });
-  await deploy('LiquidPool', { from: deployer, log: true, skipIfAlreadyDeployed: true, gasLimit: 7000000, maxPriorityFeePerGas: 5000000 });
-  await deploy('MemberToken', { from: deployer, log: true, skipIfAlreadyDeployed: true, gasLimit: 5000000, maxPriorityFeePerGas: 5000000 });
+  await deploy('Dao', { from: deployer, log: true, skipIfAlreadyDeployed: true, gasLimit: 7000000, maxFeePerGas: 100430000 });
+  await deploy('Membership', { from: deployer, log: true, skipIfAlreadyDeployed: true, gasLimit: 6000000, maxFeePerGas: 100430000 });
+  await deploy('LiquidPool', { from: deployer, log: true, skipIfAlreadyDeployed: true, gasLimit: 7000000, maxFeePerGas: 100430000 });
+  await deploy('MemberToken', { from: deployer, log: true, skipIfAlreadyDeployed: true, gasLimit: 5000000, maxFeePerGas: 100430000 });
 };
 
 module.exports.tags = [
