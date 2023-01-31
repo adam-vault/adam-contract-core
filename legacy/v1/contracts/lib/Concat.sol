5ddd0ff80734d874e4574890b099083f57d3a539
Allen Wong <allen.wong@apoidea.ai>
2023-01-31 16:13:46 +0800

    [AS-1994] remove legacy file

--- legacy/v1/contracts/lib/Concat.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0
-pragma solidity 0.8.7;
-
-library Concat {
-    function concat(string memory a, string memory b) internal pure returns(string memory) {
-        return string(abi.encodePacked(a, b));
-    }
-}
